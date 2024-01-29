1d4673f122570a48d78d3e812582dd639167c84e
TheDeadSnake <darktobi17@hotmail.de>
2024-01-29 20:10:18 +0100

    Updated version string

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ using UnityEngine.SceneManagement;
 
 namespace touchscreen;
 
-[BepInPlugin("me.pm.TheDeadSnake", "TouchScreen", "1.0.9")]
+[BepInPlugin("me.pm.TheDeadSnake", "TouchScreen", "1.0.10")]
 [BepInProcess("Lethal Company.exe")]
 [BepInDependency("LethalExpansion", BepInDependency.DependencyFlags.SoftDependency)]
 [BepInDependency("LethalExpansion(core)", BepInDependency.DependencyFlags.SoftDependency)]
